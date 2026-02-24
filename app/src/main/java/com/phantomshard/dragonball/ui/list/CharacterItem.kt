9e0b39213ac5f5c58e7a42ab5bc4d52823627aef
Dev0x1 <byte0x1@gmail.com>
2026-02-24 17:39:20 -0400

    Agrega el item de personaje.

---
+++ app/src/main/java/com/phantomshard/dragonball/ui/list/CharacterItem.kt
@@ -0,0 +1,43 @@
+package com.phantomshard.dragonball.ui.list
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.phantomshard.dragonball.domain.model.DragonBallCharacter
+
+@Composable
+fun CharacterItem(
+    character: DragonBallCharacter,
+    onClick: () -> Unit
+) {
+    ElevatedCard(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { onClick() }
+    ) {
+        Row(
+            modifier = Modifier.padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+            AsyncImage(
+                model = character.image,
+                contentDescription = character.name,
+                modifier = Modifier.size(64.dp)
+            )
+
+            Spacer(modifier = Modifier.width(16.dp))
+
+            Column {
+                Text(character.name)
+                Text("${character.race} • ${character.gender}")
+            }
+        }
+    }
+}
