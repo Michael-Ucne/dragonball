d2fceab68f880fe78efe5360ccc3c814d7d284e6
Dev0x1 <byte0x1@gmail.com>
2026-02-24 15:47:26 -0400

    Agrega Dependency Injection.

---
+++ app/src/main/java/com/phantomshard/dragonball/di/AppModule.kt
@@ -0,0 +1,46 @@
+package com.phantomshard.dragonball.di
+
+import com.phantomshard.dragonball.data.CharacterRepositoryImpl
+import com.phantomshard.dragonball.data.DragonBallApi
+import com.phantomshard.dragonball.domain.CharacterRepository
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideMoshi(): Moshi {
+        return Moshi
+            .Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideApi(moshi: Moshi): DragonBallApi {
+        val json = Json { ignoreUnknownKeys = true }
+        return Retrofit.Builder()
+            .baseUrl("https://dragonball-api.com/api/")
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            //.addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+            .build()
+            .create(DragonBallApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideRepository(api: DragonBallApi): CharacterRepository {
+        return CharacterRepositoryImpl(api)
+    }
+}
