89b2b333f862cac39cc7c70ad071578b2e38b00e
Dev0x1 <byte0x1@gmail.com>
2026-02-24 17:53:04 -0400

    agregal hilyt y navegacion

--- app/src/main/java/com/phantomshard/dragonball/MainActivity.kt
+++ app/src/main/java/com/phantomshard/dragonball/MainActivity.kt
@@ -4,44 +4,56 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.phantomshard.dragonball.ui.detail.DetailScreen
+import com.phantomshard.dragonball.ui.detail.DetailViewModel
+import com.phantomshard.dragonball.ui.list.ListScreen
 import com.phantomshard.dragonball.ui.theme.DragonBallTheme
+import com.phantomshard.dragonball.util.Screen
+import dagger.hilt.android.AndroidEntryPoint
+import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             DragonBallTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                AppNavigation()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun AppNavigation() {
+    val navController = rememberNavController()
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DragonBallTheme {
-        Greeting("Android")
+    NavHost(
+        navController = navController,
+        startDestination = Screen.List
+    ) {
+        composable<Screen.List> {
+            ListScreen(
+                onCharacterClick = { characterId ->
+                    navController.navigate(Screen.Detail(characterId))
+                }
+            )
+        }
+        composable<Screen.Detail> {
+            val viewModel: DetailViewModel = hiltViewModel()
+            val state by viewModel.state.collectAsStateWithLifecycle()
+            
+            DetailScreen(
+                state = state,
+                onBack = { navController.popBackStack() }
+            )
+        }
     }
 }
